dbbaf6e578a3a63ce3a754bd06d7c8cc179f6eec
kyle- <kyle-smith1@outlook.com>
2023-11-19 14:59:34 +0000

    Challenge 1 and 2 complete

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Xml;
+
+public class Point
+{
+    public int X { get;}
+    public int Y { get;}
+    public Point(int x, int y)
+    {
+        X = x; 
+        Y = y;
+    }
+
+    public Point()
+    {
+        X=0;
+        Y=0;
+    }
+}
+
+public class Colour
+{
+    public int Red { get; }
+    public int Green { get; }
+    public int Blue { get; }
+
+    public Colour(int red, int green, int blue){ 
+        Red = red; 
+        Green = green; 
+        Blue = blue;
+    }
+    public static Colour White(){
+        Colour white = new(255, 255, 255);
+        return white;
+    }
+    public static Colour Black(){ 
+        Colour black = new(0, 0, 0);
+        return black; 
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
 ﻿// Level 24 BOSS BATTLE -The Catacombs of the class
+//The Point
+using System.Drawing;
+
+Point p1 = new Point(2, 3);
+Point p2 = new Point(-4, 0);
+
+DisplayPoint(p1);
+DisplayPoint(p2);
+
+ChallengeEnd();
+
+// The Colour
+Colour colour1 = new(125, 125, 125);
+Colour white = Colour.White();
+
+DisplayColour(colour1);
+DisplayColour(white);
+
+ChallengeEnd();
+
+// The Card
+
+
+
+
+
+// Challenge end method
+void ChallengeEnd()
+{
+    Console.WriteLine();
+    Console.WriteLine("**********");
+}
+
+// The point
+void DisplayPoint(Point p)
+{
+    Console.WriteLine($"({p.X}, {p.Y})");
+}
+
+void DisplayColour(Colour colour)
+{
+    Console.WriteLine($"{colour.Red}, {colour.Green}, {colour.Blue}");
+}
